da368007c5d37a3d3aff905ba6c16cb79cdb56c5
PhamTuanVu31032003 <ptvu310303@gmail.com>
2024-11-14 18:41:29 +0700

    Update FormBaoCaoThongKe.Designer.cs

--- Nhom6_QuanLyThuVien/Nhom6_QuanLyThuVien/FormBaoCaoThongKe.Designer.cs
+++ Nhom6_QuanLyThuVien/Nhom6_QuanLyThuVien/FormBaoCaoThongKe.Designer.cs
@@ -92,7 +92,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(696, 149);
+            this.label2.Location = new System.Drawing.Point(691, 156);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(60, 22);
             this.label2.TabIndex = 26;
